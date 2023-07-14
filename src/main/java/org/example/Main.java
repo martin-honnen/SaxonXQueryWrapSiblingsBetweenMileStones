a953901884284525ae3f0bd482e841a496669480
Martin Honnen <martin.honnen@gmx.de>
2023-07-14 11:02:11 +0200

    Using JDOM2 Document as input and output

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
+import net.sf.saxon.option.jdom2.JDOM2ObjectModel;
 import net.sf.saxon.s9api.*;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.jdom2.transform.JDOMResult;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, SaxonApiException {
+    public static void main(String[] args) throws IOException, SaxonApiException, JDOMException {
         Processor processor = new Processor();
+
+        processor.getUnderlyingConfiguration().registerExternalObjectModel(new JDOM2ObjectModel());
+
+        Document jdomDocument = new SAXBuilder().build(new File("sample1.xml"));
 
         DocumentBuilder docBuilder = processor.newDocumentBuilder();
 
-        XdmNode inputDoc = docBuilder.build(new File("sample1.xml"));
+        XdmNode inputDoc = docBuilder.wrap(jdomDocument);
 
         XQueryCompiler xqueryCompiler = processor.newXQueryCompiler();
 
@@ -21,7 +32,15 @@ public class Main {
 
         xqueryEvaluator.setContextItem(inputDoc);
 
-        xqueryEvaluator.run(processor.newSerializer(System.out));
+        JDOMResult jdomResult = new JDOMResult();
+
+        xqueryEvaluator.run(new SAXDestination(jdomResult.getHandler()));
+
+        Document resultDoc = jdomResult.getDocument();
+
+        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+
+        xmlOutputter.output(resultDoc, System.out);
 
     }
 }
